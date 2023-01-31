634de632c52d8f28b0eea2eb6c0872dc5a06c0db
akashbokde98 <akashbokde98@gmail.com>
2023-02-01 02:34:50 +0530

    Added: Calculating Total Working Hour and Day

--- Program.cs
+++ Program.cs
@@ -9,15 +9,17 @@ namespace EmployeeWage
         int fullDayHour;
         int partTimeHour;
         int totalWorkingDay;
+        int totalWorkingHour;
         int dailyEmpWage;
 
 
-        public Program(int wage, int fullday, int parttime, int totalworkday, int iniVal)
+        public Program(int wage, int fullday, int parttime, int totalworkday, int totalhour, int iniVal)
         {
             wagePerHour = wage;
             fullDayHour = fullday;
             partTimeHour = parttime;
             totalWorkingDay = totalworkday;
+            totalWorkingHour = totalhour;
             dailyEmpWage = iniVal;
             
         }
@@ -26,32 +28,33 @@ namespace EmployeeWage
         {
             Console.WriteLine("Welcome To Employee Wage Computation Program!");
 
-            Program obj1 = new Program(20, 8, 4, 0, 0);
-            int wageForMonth = obj1.AttendanceCheck();
+            Program obj1 = new Program(20, 8, 4, 0, 0, 0);
+            int wageForMonth = obj1.EmployeeWage();
+            Console.WriteLine("Total Working Hour is " + obj1.totalWorkingHour);
+            Console.WriteLine("Total Working Days is " + obj1.totalWorkingDay);
             Console.WriteLine("Wages For a Month is " + wageForMonth);
         }
-        public int AttendanceCheck()
+        public int EmployeeWage()
         {
             int totalWage = 0;
             int day = 1;
-            while (day <= 30 && totalWorkingDay <= 20)
+            while (day <= 30 && totalWorkingDay <= 20 && totalWorkingHour < 100)
             {
                 switch (randomNum.Next(0, 3))
                 {
                     case 1:
                         dailyEmpWage = wagePerHour * fullDayHour;
-                        //Console.WriteLine("Employee is Present and Wage is " + dailyEmpWage);
                         totalWorkingDay++;
+                        totalWorkingHour += fullDayHour;
                         break;
 
                     case 2:
                         dailyEmpWage = wagePerHour * partTimeHour;
-                        //Console.WriteLine("Employee is Present Half Day and Wage is " + dailyEmpWage);
                         totalWorkingDay++;
+                        totalWorkingHour += partTimeHour;
                         break;
 
                     default:
-                        //Console.WriteLine("Employee is Absent and Wage is " + dailyEmpWage);
                         break;
                 }
                 totalWage += dailyEmpWage;
@@ -59,9 +62,5 @@ namespace EmployeeWage
             }
             return totalWage; 
         }
-        /*void TotalEmpWage()
-        {
-            
-        }*/
     }
 }
