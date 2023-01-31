cc953d92b07cb39ec1b2f8f6c984cb82791fa0a3
akashbokde98 <akashbokde98@gmail.com>
2023-02-01 00:47:38 +0530

    Added: Daily Employee Wage

--- Program.cs
+++ Program.cs
@@ -5,23 +5,36 @@ namespace EmployeeWage
     internal class Program
     {
         Random randomNum = new Random();
+        int wagePerHour;
+        int fullDayHour;
+        int dailyEmpWage;
+
+        public Program(int wage, int fullday, int iniVal)
+        {
+            wagePerHour = wage;
+            fullDayHour = fullday;
+            dailyEmpWage = iniVal;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage Computation Program!");
 
-            Program obj1 = new Program();
+            Program obj1 = new Program(20, 8, 0);
             obj1.AttendanceCheck();
+            Console.WriteLine(obj1.dailyEmpWage);
         }
         public void AttendanceCheck()
         {
             int isPresent = randomNum.Next(0, 2);
             if (isPresent == 1)
             {
-                Console.WriteLine("Employee is Present.");
+                dailyEmpWage = wagePerHour * fullDayHour;
+                Console.WriteLine("Employee is Present and Wage is " + dailyEmpWage);
             }
             else
             {
-                Console.WriteLine("Employee is Absent.");
+                Console.WriteLine("Employee is Absent and Wage is " + dailyEmpWage);
             }
         }
     }
