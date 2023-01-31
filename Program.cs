7c06b75eda335da3dcc39249fd385b739833ef8a
akashbokde98 <akashbokde98@gmail.com>
2023-02-01 01:04:32 +0530

    Added: Add part Time and Employee wage

--- Program.cs
+++ Program.cs
@@ -7,30 +7,38 @@ namespace EmployeeWage
         Random randomNum = new Random();
         int wagePerHour;
         int fullDayHour;
+        int partTimeHour;
         int dailyEmpWage;
 
-        public Program(int wage, int fullday, int iniVal)
+
+        public Program(int wage, int fullday, int parttime, int iniVal)
         {
             wagePerHour = wage;
             fullDayHour = fullday;
+            partTimeHour = parttime;
             dailyEmpWage = iniVal;
+            
         }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage Computation Program!");
 
-            Program obj1 = new Program(20, 8, 0);
+            Program obj1 = new Program(20, 8, 4, 0);
             obj1.AttendanceCheck();
-            Console.WriteLine(obj1.dailyEmpWage);
         }
         public void AttendanceCheck()
         {
-            int isPresent = randomNum.Next(0, 2);
+            int isPresent = randomNum.Next(0, 3);
             if (isPresent == 1)
             {
                 dailyEmpWage = wagePerHour * fullDayHour;
                 Console.WriteLine("Employee is Present and Wage is " + dailyEmpWage);
+            }
+            else if (isPresent == 2)
+            {
+                dailyEmpWage = wagePerHour * partTimeHour;
+                Console.WriteLine("Employee is Present Half Day and Wage is " + dailyEmpWage);
             }
             else
             {
