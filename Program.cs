160057e6aa9d99e851d74c28443190db1e5cc87f
akashbokde98 <akashbokde98@gmail.com>
2023-02-01 00:18:14 +0530

    Added: Check Employee Present/Absent

--- Program.cs
+++ Program.cs
@@ -4,9 +4,25 @@ namespace EmployeeWage
 {
     internal class Program
     {
+        Random randomNum = new Random();
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage Computation Program!");
+
+            Program obj1 = new Program();
+            obj1.AttendanceCheck();
+        }
+        public void AttendanceCheck()
+        {
+            int isPresent = randomNum.Next(0, 2);
+            if (isPresent == 1)
+            {
+                Console.WriteLine("Employee is Present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent.");
+            }
         }
     }
 }
